368b127bee0ce146b0c6e37ebb7d17348dd4f5db
niki-stadnik <niki.stadnik@gmail.com>
2023-03-20 17:12:10 +0200

    cleand up and switched to simple power chargers

--- smart-light-switch.ino
+++ smart-light-switch.ino
@@ -1,9 +1,7 @@
 #include <ArduinoJson.h>
 #include <WiFi.h>
 #include <Adafruit_AHT10.h>
-#include "CurrentSensor.h"
 #include <Arduino.h> 
-#include "ACS712.h"
 
 
 #include "config.h"
@@ -16,19 +14,9 @@ const uint16_t port = PORT;
 char * key = KEY;
 
 
-CurrentSensor sens0(12);
-CurrentSensor sens1(14);
-CurrentSensor sens2(27);
-CurrentSensor sens3(25);
-CurrentSensor sens4(33);
-CurrentSensor sens5(26);
-CurrentSensor sens6(23);
-CurrentSensor sens7(32);
 
-
-//const int currentSwitchPin[8] = {12, 14, 27, 25, 33, 26, 23, 32};
-float currentResults[8];
-
+const int powerSensorPin[8] = {12, 14, 27, 25, 33, 26, 23, 32};
+boolean powerResults[8];
 const int RelayPin[9] = {2, 15, 16, 13, 17, 5, 18, 19, 4};
 
 
@@ -49,9 +37,8 @@ boolean relay = false;
 void setup() {
   //GPIO setup
   for (int i = 0; i < 8; i++) {
-    pinMode(RelayPin[i], OUTPUT);
-    digitalWrite(RelayPin[i], LOW);
-    currentResults[i] = 0.0f;
+    pinMode(powerSensorPin[i], INPUT_PULLDOWN);
+    powerResults[i] = false;
   }
   for (int i = 0; i < 9; i++) {
     pinMode(RelayPin[i], OUTPUT);
@@ -95,14 +82,6 @@ void setup() {
     Serial.println("test mode");
   }
   */
-  sens0.auto0();
-  /*sens1.auto0();
-  sens2.auto0();
-  sens3.auto0();
-  sens4.auto0();
-  sens5.auto0();
-  sens6.auto0();
-  sens7.auto0();*/
 }
 
 void loop() {
@@ -117,44 +96,22 @@ void loop() {
   }
 */
 
-  //if(millis() >= sendtimeing + 500){
-    Serial.print("Read: 0 : ");
-    currentResults[0] = sens0.getCurrent();
-    Serial.println();/*
-    Serial.print("Read: 1 : ");
-    currentResults[1] = sens1.getCurrent();
-    Serial.println();
-    Serial.print("Read: 2 : ");
-    currentResults[2] = sens2.getCurrent();
-    Serial.println();
-    Serial.print("Read: 3 : ");
-    currentResults[3] = sens3.getCurrent();
-    Serial.println();
-    Serial.print("Read: 4 : ");
-    currentResults[4] = sens4.getCurrent();
-    Serial.println();
-    Serial.print("Read: 5 : ");
-    currentResults[5] = sens5.getCurrent();
-    Serial.println();
-    Serial.print("Read: 6 : ");
-    currentResults[6] = sens6.getCurrent();
-    Serial.println();
-    Serial.print("Read: 7 : ");
-    currentResults[7] = sens7.getCurrent();
-    Serial.println();*/
+  if(millis() >= sendtimeing + 500){
+    
 
-/*
+
     for (int i=0; i<8; i++){
-      Serial.println("Read: ");
-      Serial.println(i);
-      currentResults[i] = sens0.getCurrent();
-      Serial.println();
-    }*/
+      Serial.print("Status: ");
+      Serial.print(i);
+      Serial.print(" : ");
+      powerResults[i] = digitalRead(powerSensorPin[i]);
+      Serial.println(powerResults[i]);
+    }
 
     //sendData();
 
-    //sendtimeing = millis();
-  //}
+    sendtimeing = millis();
+  }
 
   /*
   if (!client) {
@@ -182,14 +139,14 @@ void sendData(){
   dat["fuseBoxTemp"] = temp.temperature;
   dat["fuseBoxHum"] = humidity.relative_humidity;
   dat["fuseBoxFan"] = relay;
-  dat["light0"] = currentResults[0];
-  dat["light1"] = currentResults[1];
-  dat["light2"] = currentResults[2];
-  dat["light3"] = currentResults[3];
-  dat["light4"] = currentResults[4];
-  dat["light5"] = currentResults[5];
-  dat["light6"] = currentResults[6];
-  dat["light7"] = currentResults[7];
+  dat["light0"] = powerResults[0];
+  dat["light1"] = powerResults[1];
+  dat["light2"] = powerResults[2];
+  dat["light3"] = powerResults[3];
+  dat["light4"] = powerResults[4];
+  dat["light5"] = powerResults[5];
+  dat["light6"] = powerResults[6];
+  dat["light7"] = powerResults[7];
 
   
   char json[1024];
